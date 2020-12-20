9001540391f4538114400bbf48077c5b8f2d4567
VaneSuckoski <74139708+VaneS2@users.noreply.github.com>
2020-12-20 20:02:35 +0100

    final
    https://global-health.herokuapp.com

---
+++ Домашна 3/Domasna2/src/main/java/com/example/demo/web/JazikController.java
@@ -0,0 +1,33 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/jazik")
+public class JazikController {
+
+    @GetMapping("/en")
+    public String smenijazik(HttpSession httpServletRequest)
+    {
+        httpServletRequest.setAttribute("en",1);
+
+        return "home";
+
+    }
+
+    @GetMapping("/mk")
+    public String smenijaziks(HttpSession httpServletRequest)
+    {
+        httpServletRequest.setAttribute("en",null);
+
+        return "home";
+
+    }
+
+
+}
---
+++ Домашна 3/Domasna2/src/main/java/com/example/demo/web/Lokator.java
@@ -0,0 +1,66 @@
+package com.example.demo.web;
+
+import com.example.demo.Service.AptekaService;
+import com.example.demo.Service.MapService;
+import com.example.demo.model.Apteka;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequestMapping("/lokator")
+public class Lokator {
+    private final MapService mapService;
+    private final AptekaService aptekaService;
+
+    public Lokator(MapService mapService, AptekaService aptekaService) {
+        this.mapService = mapService;
+        this.aptekaService = aptekaService;
+    }
+
+
+    @GetMapping
+    public String lokator(HttpSession httpSession,Model model)
+    {
+        model.addAttribute("lokacii",aptekaService.findallPharmacy());
+
+        return "primer";
+    }
+
+    @GetMapping("/search")
+    public String search(HttpServletRequest httpServletRequest, Model model, @RequestParam  String search)
+    {
+
+
+        List<Apteka> lista=aptekaService.findbyC(search);
+        if(lista.size()==0)
+        {
+            String [] niza=search.split("");
+            niza[0]= niza[0].toUpperCase();
+            String cc="";
+            for(int i=0;i<niza.length;i++)
+            {
+                cc+=niza[i];
+            }
+
+            lista=aptekaService.findbyC(cc);
+
+        }
+        model.addAttribute("lokacii",lista);
+
+        model.addAttribute("ZaFokus",true);
+
+
+        model.addAttribute("prebarano",search);
+        System.out.println(aptekaService.findbyC(search));
+
+        return "primer";
+    }
+
+}
